87dfad3125e6e2db0929ed0e57230d558d55f262
CarmenKing8 <carmen88martinezmartinez@gmail.com>
2026-05-21 10:49:27 +0200

    primer anadido back

---
+++ backend/src/main/java/com/example/backend/model/Autor.java
@@ -0,0 +1,40 @@
+package com.example.backend.model;
+
+public class Autor {
+    private Long id;
+
+    private String nombre;
+    private String nacionalidad;
+
+    public Autor() {
+    }
+
+    public Autor(String nombre, String nacionalidad) {
+        this.nombre = nombre;
+        this.nacionalidad = nacionalidad;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+}
